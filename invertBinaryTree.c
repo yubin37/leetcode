9d5fc802e93d92fe12513355fb0fc08f5973dd73
Yubin Yang <yubin@cnyubin.com>
2017-01-29 22:47:42 +0800

    Add the solution of Invert Binary Tree

---
+++ invertBinaryTree.c
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * }
+ */
+struct TreeNode* invertTree(struct TreeNode* root) {
+	if (root == NULL)
+		return NULL;
+	struct TreeNode* temp = root->left;
+	root->left = root->right;
+	root->right = temp;
+
+	invertTree(root->right);
+	invertTree(root->left);
+	
+	return root;
+}
